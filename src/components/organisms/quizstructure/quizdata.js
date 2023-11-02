4e94c7b9de8438db2aea7cd49ddf01cd5afb58a5
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-11-02 23:45:26 +0530

    chnage question

--- src/components/organisms/quizstructure/quizdata.js
+++ src/components/organisms/quizstructure/quizdata.js
@@ -11,32 +11,32 @@ export  const Quizdata=[{
     options:["Monday","Tuesday","Wednesday","Thursday"],
     answer:3
 },{
-    question:"What will be the day of the week 15th August, 2010?",
-    options:["Sunday","Monday","Tuesday","Friday"],
+    question:"What is the value of Pi to four individual decimal places?",
+    options:["3.1416","32.1416","3.51416","3.2"],
     answer:1
 },{
-    question:"Today is Monday. After 61 days, it will be:",
-    options:["Wednesday","Saturday" ,"Tuesday ","Thursday"   ],
+    question:"What does 6 raise to the power of 0 equal?",
+    options:["6","1" ,"0 ","36"   ],
     answer:2
 },{
-    question:"It was Sunday on Jan 1, 2006. What was the day of the week Jan 1, 2010?",
-    options:["Sunday","Saturday","Friday","Wednesday"],
-    answer:3
-},{
-    question:"What was the day of the week on 28th May, 2006?",
-    options:["Thrusday","Friday","Saturday","Sunday"],
+    question:"Solve the following equation: -15 + (-5x)=0. ",
+    options:["1","3","0","-3"],
     answer:4
 },{
-    question:"What was the day of the week on 17th June, 1998?",
-    options:["Monday","Tuesday","Wednesday","Thursday"],
-    answer:3
-},{
-    question:"What will be the day of the week 15th August, 2010?",
-    options:["Sunday","Monday","Tuesday","Friday"],
+    question:"A car is traveling at the rate of 75 kilometers per hour. How many meters is the car traveling in one minute?",
+    options:["1250 m/min","250 m/min","1856 m/min","522 m/min"],
     answer:1
 },{
-    question:"Today is Monday. After 61 days, it will be:",
-    options:["Wednesday","Saturday" ,"Tuesday ","Thursday"   ],
+    question:"How many hours are there in a year (rounded to the nearest hour)?",
+    options:["4596 hours","8760 hours","2556 hours","2159 hours"],
+    answer:2
+},{
+    question:" Which number is best represented in binary as 100? ",
+    options:["1","6","4","2"],
+    answer:3
+},{
+    question:"60 Times of 8 Equals to",
+    options:["481","480" ,"490","482"],
     answer:2
 }];
 
